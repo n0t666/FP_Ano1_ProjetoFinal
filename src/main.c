550828f74ff11238e231be3019b7ebe608759dee
Leandro Pereira <opereira3591@gmail.com>
2023-11-30 19:52:33 +0000

    initial commit

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+
+#define MAXIMO_ESTUDANTES 150
+#define MAXIMO_AVALIACOES 5000
+#define UNIDADES_CURRICULARES 18
+
+
+void menu_principal();
+
 
 int main()
 {
-    printf("Hello world!\n");
+
+    setlocale(LC_ALL,"Portuguese");
+
+
     return 0;
 }
